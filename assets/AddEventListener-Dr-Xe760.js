77c7751c0fa496ca3a4a85155258a8e947638754
ShankarAswal <shankaraswal@gmail.com>
2026-09-13 00:23:02 +0100

    Updates

---
+++ assets/AddEventListener-Dr-Xe760.js
@@ -0,0 +1,1 @@
+import{r as n,j as r}from"./index-CJ-cxNaP.js";function s(){return n.useEffect(()=>{const e=()=>{console.log("Window resized")};return window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}},[]),r.jsx("div",{children:"My Component"})}export{s as default};
